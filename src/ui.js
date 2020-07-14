3bc6d00ff29eb1052396540e0c2561e7e234c651
Ryosuke <ryosuke.san.hana@gmail.com>
2020-07-14 13:23:44 -0700

    :fire: Remove old JS

--- src/ui.js
+++
@@ -1,8 +0,0 @@
-import "figma-plugin-ds/dist/figma-plugin-ds.css";
-document.getElementById("copy").onclick = () => {
-    parent.postMessage({ pluginMessage: { type: "copy" } }, "*");
-};
-document.getElementById("generate").onclick = () => {
-    const textbox = document.getElementById("theme");
-    parent.postMessage({ pluginMessage: { type: "generate", theme: textbox.innerHTML } }, "*");
-};
